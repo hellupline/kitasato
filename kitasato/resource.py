3f25cdb2cb3b4754e3deaa8caac20b63e77f754d
Renan Traba <hellupline@gmail.com>
2016-06-28 16:21:31 -0300

    Add Controller Interface

--- kitasato/resource.py
+++ kitasato/resource.py
@@ -6,10 +6,115 @@
 
     This module implements a simple RPC interface to help create HTTP APIs.
 """
+from functools import reduce
+import operator as op
+
 from kitasato import tree, component
 
 
-class Resource(tree.Tree):
+def compose(*funcs):
+    return lambda x: reduce(lambda v, f: f(v), reversed(funcs), x)
+
+
+class ControllerMixin:
+    per_page = 50
+    filters = None
+
+    def get_items(self, page=1, order_by=None, reverse=False, filters=None):
+        """Combines the methods:
+            - `fetch_items`
+            - `filter_items`
+            - `sort_items`
+            - `slice_items`
+            - `count_items`
+
+        Arguments:
+            page (int): the page number
+            filters (sequence): a sequence of 2-items tuple of
+                                (filter_key, value)
+            order_by (str): the field to order items by
+            reverse (bool): reverse the sort order,
+                            only if order_by is not None
+        """
+        items = self.fetch_items()
+        if filters is not None:
+            items = self.filter_items(items, filters=filters)
+        if order_by is not None:
+            items = self.sort_items(items, order_by=order_by, reverse=reverse)
+        items = self.slice_items(items, page=page)
+        return items, self.count_items(items)
+
+    def filter_items(self, items, filters):
+        """Filter items based on filters in `filters` attribute.
+
+        Arguments:
+            items (sequence): the items returned from `fetch_items`
+            filters (sequence): a sequence of 2-items tuple of
+                                (filter_key, value)
+        Returns:
+            list: filtered list
+        """
+        for filter_key, filter_value in filters:
+            try:
+                filter_func = self.filters[filter_key]
+            except KeyError:
+                continue
+            filter_func(filter_value, items)
+        return items
+
+    def sort_items(self, items, order_by=None, reverse=False):
+        """Sort items based on `order_by` key in items.
+
+        Arguments:
+            items (sequence): the items returned from `fetch_items`
+            order_by (str): the field to order items by
+            reverse (bool): reverse the sort order
+
+        Returns:
+            list: sorted items by `order_by`
+        """
+        if order_by is None:
+            return sorted(items, reverse=reverse)
+        return sorted(items, key=op.itemgetter(order_by), reverse=reverse)
+
+    def slice_items(self, items, page=1):
+        """Slice items in `per_page` items.
+
+        Arguments:
+            items (sequence): the items returned from `fetch_items`
+            page (int): the page number
+
+        Returns:
+            list: slice of items
+        """
+        start, end = self.per_page*(page-1), self.per_page*page
+        return items[start:end]
+
+    def count_items(self, items):
+        """Items size.
+
+        Returns:
+            int: len of items
+        """
+        return len(items)
+
+    def fetch_items(self):
+        raise NotImplementedError
+
+    def get_item(self, pk):
+        raise NotImplementedError
+
+    def create_item(self, data):
+        raise NotImplementedError
+
+    def update_item(self, item, data):
+        raise NotImplementedError
+
+    def delete_item(self, item):
+        raise NotImplementedError
+
+
+class Resource(ControllerMixin, tree.Tree):  # pylint: disable=abstract-method
     """Simple RPC APIs may implements a CRUD interface.
 
     This class is a :class:`kitasato.tree.Tree` with predetermined items:
