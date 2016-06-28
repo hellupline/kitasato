f8e0be7b22bb2618f34aab1c2a39b9db88fe1af8
Renan Traba <hellupline@gmail.com>
2016-06-28 01:07:28 -0300

    more lazy aproach of endpoint_map and url_map

--- kitasato/application.py
+++ kitasato/application.py
@@ -1,3 +1,4 @@
+from cached_property import cached_property
 from werkzeug import routing, exceptions
 
 from kitasato import tree, response
@@ -8,11 +9,17 @@ NO_ENDPOINT_MSG = 'Endpoint not found.'
 class Application(response.RequestDispatcher, tree.Tree):
     def __init__(self, name, items):
         super().__init__(name=name, items=items)
-        self.url_map = routing.Map([self.get_url_rules()])
-        self.endpoint_map = dict(self.get_endpoints())
+
+    @cached_property
+    def url_map(self):
+        return routing.Map([self.get_url_rules()])
+
+    @cached_property
+    def endpoint_map(self):
+        return dict(self.get_endpoints())
 
     def dispatch_request(self, request):
-        adapter = self.url_map.bind_to_environ(request.environ)
+        adapter = self.get_url_adapter(request)
         try:
             endpoint, values = adapter.match()
             return self.serve_endpoint(request, endpoint, values)
@@ -20,6 +27,9 @@ class Application(response.RequestDispatcher, tree.Tree):
             return e
         except exceptions.HTTPException as e:  # pragma: no cover
             return e
+
+    def get_url_adapter(self, request):
+        return self.url_map.bind_to_environ(request.environ)
 
     def serve_endpoint(self, request, endpoint, values):
         try:
